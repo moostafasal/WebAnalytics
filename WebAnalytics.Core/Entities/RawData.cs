b584b672a36401788d196956e7f6b57ddd6c3f11
Mostafa Salem <mostafaasalem2@gmail.com>
2025-11-03 22:49:57 +0200

    Add RawData entity

---
+++ WebAnalytics.Core/Entities/RawData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAnalytics.Core.Entities
+{
+    public class RawData
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Page { get; set; } = string.Empty;
+        public int Users { get; set; }
+        public int Sessions { get; set; }
+        public int Views { get; set; }
+        public double PerformanceScore { get; set; }
+        public int LCPms { get; set; }
+        public DateTime ReceivedAt { get; set; } = DateTime.UtcNow;
+    }
+}
