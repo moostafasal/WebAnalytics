e88066be782f3e876ac7e1d11985ccaccee6e143
Mostafa Salem <mostafaasalem2@gmail.com>
2025-11-04 12:57:14 +0200

    change sqlServer to postgrSQL

--- WebAnalytics.Core/Entities/DailyStats.cs
+++ WebAnalytics.Core/Entities/DailyStats.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace WebAnalytics.Core.Entities
 {
-    internal class DailyStats
+    public class DailyStats
     {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int TotalUsers { get; set; }
+        public int TotalSessions { get; set; }
+        public int TotalViews { get; set; }
+        public double AvgPerformance { get; set; }
+        public DateTime LastUpdatedAt { get; set; } = DateTime.UtcNow;
     }
+
 }
---
+++ WebAnalytics.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAnalytics.Core.Entities;
+
+namespace WebAnalytics.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<RawData> RawData { get; set; }
+        public DbSet<DailyStats> DailyStats { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.HasIndex(e => e.Email).IsUnique();
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.PasswordHash).IsRequired();
+            });
+
+            modelBuilder.Entity<RawData>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.HasIndex(e => new { e.Date, e.Page });
+                entity.Property(e => e.Page).IsRequired().HasMaxLength(200);
+            });
+
+            modelBuilder.Entity<DailyStats>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.HasIndex(e => e.Date).IsUnique();
+            });
+        }
+    }
+}
