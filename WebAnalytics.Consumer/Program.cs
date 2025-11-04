9a30a9446721cb3f660fe95028fc75245c838116
Mostafa Salem <mostafaasalem2@gmail.com>
2025-11-04 15:44:18 +0200

    connction string setting and add program config

--- WebAnalytics.Consumer/Program.cs
+++ WebAnalytics.Consumer/Program.cs
@@ -1,14 +1,23 @@
-namespace WebAnalytics.Consumer
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = Host.CreateApplicationBuilder(args);
-            builder.Services.AddHostedService<Worker>();
+using Microsoft.EntityFrameworkCore;
+using WebAnalytics.Consumer;
+using WebAnalytics.Infrastructure.Data;
+using WebAnalytics.Infrastructure.MessageBroker;
+using WebAnalytics.Infrastructure.Services;
 
-            var host = builder.Build();
-            host.Run();
-        }
-    }
-}
+var builder = Host.CreateApplicationBuilder(args);
+
+// Add database services
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add RabbitMQ services
+builder.Services.AddSingleton<RabbitMQService>();
+
+// Add application services
+builder.Services.AddScoped<IAnalyticsService, AnalyticsService>();
+
+// Add Worker
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
